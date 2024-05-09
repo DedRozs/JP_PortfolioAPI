9adab3b4dc326779632f3a30b5ea1f97ccdcda0c
Joseph Prince <josephprince1203@gmail.com>
2024-05-08 21:26:29 -0600

    Created an index view for the api app and added included routing

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
 
 # Create your views here.
+
+class Index(APIView):
+    # permission_classes = [IsAuthenticated]
+    """
+    List all partners, or create a new partner.
+    """
+    def get(self, request, format=None):
+       
+        return Response({"Message":"Some Data"}, status=status.HTTP_201_CREATED)
+
+    # def post(self, request, format=None):
+    #     serializer = PartnerSerializer(data=request.data, context={"request":request})
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- core/settings.py
+++ core/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "rest_framework",
+    "apps.api",
 ]
 
 MIDDLEWARE = [
