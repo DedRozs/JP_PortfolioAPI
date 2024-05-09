a4a17ae8085838f641b12d7c6c25bb5039c19bfa
Joseph Prince <josephprince1203@gmail.com>
2024-05-08 20:47:25 -0600

    Added static and mediafiles directories

--- core/settings.py
+++ core/settings.py
@@ -127,16 +127,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_ROOT = os.path.join(CORE_DIR, 'static')
+STATIC_ROOT = '/static'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(CORE_DIR, 'apps/static'),
+    '/static',
 )
 
 MEDIA_URL = '/mediafiles/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'apps/mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
